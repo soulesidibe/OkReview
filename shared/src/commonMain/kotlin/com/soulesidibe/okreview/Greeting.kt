97cb8435e12534ec462074db5e6c43e2bd9a5f2c
Souleymane Sidibe <souleymane.sidibe01@gmail.com>
2022-03-30 17:58:34 +0000

    init

---
+++ shared/src/commonMain/kotlin/com/soulesidibe/okreview/Greeting.kt
@@ -0,0 +1,7 @@
+package com.soulesidibe.okreview
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/soulesidibe/okreview/Platform.kt
@@ -0,0 +1,5 @@
+package com.soulesidibe.okreview
+
+expect class Platform() {
+    val platform: String
+}
